9cb539a8b01a1c24819f4c2e6eed4fca23ed1cb5
Mario Mucalo <mucalo.mario@gmail.com>
2018-10-24 10:51:05 +0200

    Add project files.

---
+++ RoslynDecisionMakerDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoslynDecisionMakerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
